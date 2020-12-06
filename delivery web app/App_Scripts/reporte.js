b0418d4188b48eb30a5fd511e64c8af02e534eb4
alejandro <alejandro-rdz-@hotmail.com>
2020-12-06 15:15:20 -0500

    delivery asp net framework

---
+++ delivery web app/App_Scripts/reporte.js
@@ -0,0 +1,21 @@
+﻿function myFunction() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    var select = document.getElementById("selccionar");
+
+    
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[select.value];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
